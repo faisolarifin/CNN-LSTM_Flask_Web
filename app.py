89165d275dee7a894b8d7a105783603e8e8f1c25
Ach. Faisol S. Arifin <faisolofficial99@gmail.com>
2022-07-20 21:54:44 +0700

    update

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, abort
 
 import numpy as np
-#from keras.preprocessing import image
-#from keras.models import load_model
+from keras.preprocessing import image
+from keras.models import load_model
 
 
 app = Flask(__name__)
 
 target_class = ['COVID-19', 'Normal', 'Pneumonia']
-# my_model = load_model("model/A_model_fold-1.h5")
+my_model = load_model("model/A_model_fold-1.h5")
 
 imgs = 'static/img.jpg'
 
@@ -26,6 +26,15 @@ def normalization(image):
  image = ((image - np.min(image)) / (np.max(image) - np.min(image)))
  return image
 
+def myPredictor():
+  if os.path.exists(imgs):
+    img = image.load_img(imgs, target_size=(224,224))
+    img = image.img_to_array(img)
+    img = normalization(img)
+    img = img.reshape(1,224,224,3)
+    preds = my_model.predict(img)
+    i = np.argmax(preds[0])
+    return target_class[i], '{:.2f}'.format(preds[0][i]*100)
 
 @app.route("/")
 def home():
@@ -38,6 +47,7 @@ def upload_file():
   if uploaded_file.filename != '':
     uploaded_file.save(imgs)
     
-  return render_template('index.html')
+  predict = myPredictor()
+  return render_template('index.html', predict=predict[0], score=predict[1])
     
 app.run(debug=True)
