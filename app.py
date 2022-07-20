1859f22dca3abf7cea10f89eaa90f1b3e2fcd2bd
Ach. Faisol S. Arifin <faisolofficial99@gmail.com>
2022-07-21 06:04:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,22 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, abort
 import numpy as np
 
-# from keras.preprocessing import image
-# from keras.models import load_model
+from keras.preprocessing import image
+from keras.models import load_model
 
 project_root = os.path.dirname(__file__)
 template_path = os.path.join(project_root, 'templates')
 static_path = os.path.join(project_root, 'static')
-
+model_path = os.path.join(project_root, 'model')
+print(model_path)
 
 app = Flask(__name__, template_folder=template_path, static_folder=static_path)
 app.secret_key = 'ini kunci rahasia'
 
-# target_class = ['COVID-19', 'Normal', 'Pneumonia']
-# my_model = load_model("model/A_model_fold-1.h5")
+target_class = ['COVID-19', 'Normal', 'Pneumonia']
+my_model = load_model(f"{model_path}/A_model_fold-1.h5")
 
-# imgs = 'static/img.jpg'
+imgs = f"{static_path}/img.jpg"
 
 # No caching at all for API endpoints.
 @app.after_request
@@ -27,9 +28,9 @@ def add_header(response):
     response.headers['Expires'] = '-1'
     return response
 
-# def normalization(image):
-#  image = ((image - np.min(image)) / (np.max(image) - np.min(image)))
-#  return image
+def normalization(image):
+ image = ((image - np.min(image)) / (np.max(image) - np.min(image)))
+ return image
 
 # def myPredictor():
 #   if os.path.exists(imgs):
